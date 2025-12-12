a7338b8cc5507bc0737063658b4493261a08d5cb
mmdonohue <mm_donohue@yahoo.com>
2025-12-11 16:18:31 -0800

    email testing

--- server/config/email.ts
+++ server/config/email.ts
@@ -5,10 +5,12 @@ const mailcreds = {
   host: process.env.SMTP_HOST || 'mg.seravanna.com',
   port: parseInt(process.env.SMTP_PORT || '465', 10),
   secure: true,
+  authMethod: process.env.SMTP_AUTH_METHOD || 'LOGIN',
   auth: {
     user: process.env.SMTP_USER || 'user@example.com',
     pass: process.env.SMTP_PASSWORD || 'password',
-  }
+  },
+  connectionTimeout: 10000
 };
 
 
