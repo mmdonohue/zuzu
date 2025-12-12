88ec89c4c86a4b076c3a60383b16b9beed2558ed
mmdonohue <mm_donohue@yahoo.com>
2025-12-11 16:38:35 -0800

    more email debug

--- server/config/email.ts
+++ server/config/email.ts
@@ -1,16 +1,21 @@
 // server/config/email.ts
+import { log } from 'console';
 import nodemailer from 'nodemailer';
 
 const mailcreds = {
   host: process.env.SMTP_HOST || 'mg.seravanna.com',
-  port: parseInt(process.env.SMTP_PORT || '465', 10),
+  port: process.env.SMTP_PORT || 465,
   secure: true,
   authMethod: process.env.SMTP_AUTH_METHOD || 'LOGIN',
   auth: {
     user: process.env.SMTP_USER || 'user@example.com',
     pass: process.env.SMTP_PASSWORD || 'password',
   },
-  connectionTimeout: 10000
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  logger: true,
+  debug: false
 };
 
 
